da9c2a107c0d7acb1c2d4a60483fa2ef6c6f209a
Pavel Stoyanov <fokervt19@gmail.com>
2024-04-06 23:02:38 +0300

    assignment

--- demo-project/src/app/app.module.ts
+++ demo-project/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
 import { ServerComponent } from './server/server.component';
+import { SuccessAlert } from './success-alert/success-alert.component';
+import { WarningAlert } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServersComponent,
-    ServerComponent
+    ServerComponent,
+    SuccessAlert,
+    WarningAlert
   ],
   imports: [
     BrowserModule
---
+++ demo-project/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "waring-alert",
+    templateUrl: "./warning-alert.component.html",
+    styleUrl: "./warning-alert.component.css"
+})
+export class WarningAlert {
+
+}
