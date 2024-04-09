a559731300a3fc88d498fa386b5f390b6571f29e
lilpaf <fokervt19@gmail.com>
2024-04-09 16:38:19 +0300

    Local references

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -7,11 +7,12 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class CockpitComponent {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  @Output() blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent: string}>();
   newServerName = '';
   newServerContent = '';
  
-  onAddServer() {
+  onAddServer(nameInput) {
+    console.log(nameInput)
     this.serverCreated.emit({
       serverName: this.newServerName, 
       serverContent: this.newServerContent});
