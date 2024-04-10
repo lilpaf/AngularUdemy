6d890a864419fd69546cfcf0767fe131f6e42b7a
Pavel Stoyanov <fokervt19@gmail.com>
2024-04-10 14:01:53 +0300

    Started assignment

---
+++ property-event-binding/src/app/game-control/game-control.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrl: './game-control.component.css'
+})
+export class GameControlComponent {
+  count : number = 1;
+
+  incrementCount() {
+    this.count++;
+  }
+
+  onGameStart() {
+    setInterval(this.incrementCount, 1000);
+  }
+}
