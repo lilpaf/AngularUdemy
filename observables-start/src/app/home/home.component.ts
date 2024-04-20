8aebfc3e41d52f548dfd88c8683401fe0fe00f66
Pavel Stoyanov <fokervt19@gmail.com>
2024-04-20 20:48:57 +0300

    Observables start

---
+++ observables-start/src/app/home/home.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, interval, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  private firsObsSubscription: Subscription;
+
+  constructor() {}
+
+  ngOnInit() {
+    // this.firsObsSubscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+    const customIntervalObservable = new Observable((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count === 5) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is grater than 3!'));
+        }
+        count++;
+      }, 1000);
+    });
+
+    // const customIntervalObservable = Observable.create((observer) => {
+    //   let count = 0;
+    //   setInterval(() => {
+    //     observer.next(count);
+    //     count++;
+    //   }, 1000);
+    // });
+
+    customIntervalObservable.pipe(map((data: number) => {
+      return `Round ${data + 1}`;
+    }));
+
+    this.firsObsSubscription = customIntervalObservable.subscribe(
+      (data: number) => {
+        console.log(data);
+      },
+      (error: Error) => {
+        console.log(error);
+        alert(error.message);
+      }, 
+      () => {
+        console.log('Completed!');
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.firsObsSubscription.unsubscribe();
+  }
+}
