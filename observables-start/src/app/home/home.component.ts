2e8356215e6d095eb59c87797218dc1449f34bb8
Pavel Stoyanov <fokervt19@gmail.com>
2024-04-20 21:14:36 +0300

    Subjects

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable, interval, Subscription } from 'rxjs';
-import { map } from 'rxjs/operators';
-
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -39,22 +38,27 @@ export class HomeComponent implements OnInit, OnDestroy {
     //   }, 1000);
     // });
 
-    customIntervalObservable.pipe(map((data: number) => {
-      return `Round ${data + 1}`;
-    }));
-
-    this.firsObsSubscription = customIntervalObservable.subscribe(
-      (data: number) => {
-        console.log(data);
-      },
-      (error: Error) => {
-        console.log(error);
-        alert(error.message);
-      }, 
-      () => {
-        console.log('Completed!');
-      }
-    );
+    this.firsObsSubscription = customIntervalObservable
+      .pipe(
+        filter((data: number) => {
+          return data > 0;
+        }),
+        map((data: number) => {
+          return `Round ${data + 1}`;
+        })
+      )
+      .subscribe(
+        (data: string) => {
+          console.log(data);
+        },
+        (error: Error) => {
+          console.log(error);
+          alert(error.message);
+        },
+        () => {
+          console.log('Completed!');
+        }
+      );
   }
 
   ngOnDestroy(): void {
